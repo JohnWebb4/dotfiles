09d860beee91cdd581e0bc99bf46ed50e51aa284
John <John.Webb94@gmail.com>
2019-06-18 21:59:30 -0700

    Add mp4 to frame converter

---
+++ Documents/bin/python/mp42img.py
@@ -0,0 +1,23 @@
+import cv2
+import sys
+
+filename = sys.argv[1] if len(sys.argv) > 1 else ""
+
+if not filename.strip():
+    sys.exit("Missing video to read");
+
+
+video = cv2.VideoCapture(filename)
+
+success, image = video.read()
+count = 0
+
+if not success:
+    sys.exit("Failed to open file")
+
+while success:
+    cv2.imwrite("frame%d.jpg" % count, image)
+    success, image = video.read()
+
+    print("Completed frame ", count)
+    count += 1
